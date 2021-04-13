0ed4cfa20e2442a1ce65f82c0ea2de89d51166e9
Huelbert Rodrigo <rodrigo@scontti.com.br>
2021-04-13 13:47:48 -0300

    feat: add naming strategy typeorm

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,4 +1,5 @@
-import { ConnectionOptions } from 'typeorm'
+import { ConnectionOptions, SimpleConsoleLogger } from 'typeorm'
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies'
 
 import { DB_HOST, DB_PORT, DB_BASE, DB_USER, DB_PASS } from '../utils/constants'
 
@@ -9,10 +10,13 @@ const config: ConnectionOptions = {
   database: DB_BASE,
   username: DB_USER,
   password: DB_PASS,
-  synchronize: false,
   logging: process.env.NODE_ENV === 'development',
+  logger: new SimpleConsoleLogger(process.env.NODE_ENV === 'development'),
+  synchronize: false,
+  migrationsRun: true,
   entities: ['src/entities/**/*.ts'],
   migrations: ['src/database/migrations/**/*.ts'],
+  namingStrategy: new SnakeNamingStrategy(),
   cli: {
     entitiesDir: 'src/entities',
     migrationsDir: 'src/database/migrations'
