95866ac0ba94b9099f6df2bec8efa6739e13b31b
Huelbert Rodrigo <rodrigo@scontti.com.br>
2021-04-13 20:10:10 -0300

    feat: add reservation test

---
+++ tests/resources/reservation-seeder.ts
@@ -0,0 +1,36 @@
+import { getManager } from 'typeorm'
+
+import { studentId } from '../resources/student-seeder'
+import { bookId } from '../resources/book-seeder'
+
+export const reservationId = '190a1b2a-66f5-4ed6-859b-8f411d9ce170'
+
+export const reservationData = {
+  id: reservationId,
+  studentId: studentId,
+  bookId: bookId,
+  reservationDate: '2021-01-01',
+  returnDate: '2021-01-08'
+}
+
+export function createReservation(): Promise<any> {
+  const reservation = [
+    reservationData.id,
+    reservationData.studentId,
+    reservationData.bookId,
+    reservationData.reservationDate,
+    reservationData.returnDate,
+    new Date(), // createdAt
+    new Date() // updatedAt
+  ]
+
+  const sql = `INSERT INTO RESERVATIONS 
+  (id, student_id, book_id, reservation_date, return_date, created_at, updated_at) VALUES 
+  ($1, $2, $3, $4, $5, $6, $7)`
+
+  return getManager().query(sql, reservation)
+}
+
+export function clearTableReservation(): Promise<any> {
+  return getManager().query('DELETE FROM RESERVATIONS')
+}
